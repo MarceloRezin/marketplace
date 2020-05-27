bc877634808596878593fd0a29aab215c16e129c
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-27 16:22:19 -0300

    Api de avaliações

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const db = require("./api/data/db");
 const rotaCategorias = require("./api/routes/categorias");
 const rotaUsuarios = require("./api/routes/usuarios");
 const rotaAnuncios = require("./api/routes/anuncios");
+const rotaAvaliacoes = require("./api/routes/avaliacoes");
 
 app.use(express.json());
 
 app.use("/categorias", rotaCategorias);
 app.use("/usuarios", rotaUsuarios);
 app.use("/anuncios", rotaAnuncios);
+app.use("/avaliacoes", rotaAvaliacoes);
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000.');
