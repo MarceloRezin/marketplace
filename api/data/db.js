e664ec222f3a2bb72d7d485b85dcbdc320f51993
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-27 13:36:07 -0300

    Parametro para evitar warning

--- api/data/db.js
+++ api/data/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://marcelo-rezin:vDviRN83@marcelorezin.com.br:23151/marketplace", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://marcelo-rezin:vDviRN83@marcelorezin.com.br:23151/marketplace", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 mongoose.connection.on("connected", () =>{
     console.log("Conexão com o banco inciada.");
