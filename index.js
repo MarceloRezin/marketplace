df785fd5362765a31cc3d3a98c9ad2cc5785677d
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-27 15:58:10 -0300

    Api de anúncios

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const db = require("./api/data/db");
 
 const rotaCategorias = require("./api/routes/categorias");
 const rotaUsuarios = require("./api/routes/usuarios");
+const rotaAnuncios = require("./api/routes/anuncios");
 
 app.use(express.json());
 
 app.use("/categorias", rotaCategorias);
 app.use("/usuarios", rotaUsuarios);
+app.use("/anuncios", rotaAnuncios);
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000.');
