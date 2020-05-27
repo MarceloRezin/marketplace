14117de6b2a7f64fb2adc346f71a3e25ae204578
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-27 13:37:59 -0300

    Adição da rota de categorias

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const db = require("./api/data/db");
 
+const rotaCategorias = require("./api/routes/categorias");
+
+app.use(express.json());
+
+app.use("/categorias", rotaCategorias);
+
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000.');
 });
