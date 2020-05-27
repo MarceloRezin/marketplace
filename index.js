62ec220902739d630af48d93b3d1525ae1871659
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-27 13:37:33 -0300

    Conexão com o banco e ajuste da mensagem

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const db = require("./api/data/db");
 
 app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000');
+    console.log('Aplicação executando na porta 3000.');
 });
